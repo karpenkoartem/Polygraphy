7461bed50575621ef296dd729701c71534044f26
karpenkoartem <art_k@i.ua>
2012-04-15 20:42:17 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polygraphy::Application.config.secret_token = 'f3579d3622f2f664e0ab75a8f44f79b14e2cdbd8b0aaa2bdb1f1412860b72b5db9430c9d388523a303a563fb9e2a4a644e2896676d43cc6d47138081aab2a6a5'
